ab57e85f908d3dd039171b397aa259cb8ce174a7
codingbykelly <42163020+codingbykelly@users.noreply.github.com>
2018-08-19 03:11:26 -0400

    Add files via upload

---
+++ fp/index.php
@@ -0,0 +1,110 @@
+
+<!DOCTYPE html >
+
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Crystal Urbinato</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="default.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="header">
+	<h1>Crystal Urbinato</h1>
+	<h2>Planner Collector-Nail Technician-Super Mom</h2>
+    
+    
+</div>
+<div id="menu">
+	<ul>
+		<li class="first"><a href="index.php">Home</a></li>
+	
+		
+		
+	</ul>
+</div>
+<div id="content">
+	<div id="columnA">
+		<h2>Planner Collector-Nail Technician-Super Mom</h2>
+        
+        <img src="images/images44444.jpg" class="desktop" alt="" />
+       
+        
+		<p>After living on my own in NY from 2005 until 2009, I decided one day to hop on a plane and move to Puerto Rico. I arrived at 12.15pm August 29, 2009.
+
+ 
+
+I attempted to study accounting at the Inter-American University of Aguadilla. But  I was bored with some of the classes having taken them in High school. So I left the university and signed up for Beauty School.
+
+ 
+
+I started My Angel Beauty School for nail technician in January of 2011 without being able to speak Spanish. I refused to learn before then.<p/>
+
+<p>My first professor at beauty school did not speak English and although the school offered me English textbooks I refused and decided to learn Spanish. So while I was learning how to do nails I was also learning Spanish.
+
+Beauty school was harder than you think. I had to learn so many things about the body and infections it took almost 6 months of information before we finally learned a basic manicure. And before I knew it we were practicing acrylics and gels learning hand designs and 3d art. This turned into me discovering a passion I never knew existed. In July 2012 I graduated high honor ( I aced every test and practice) as a certified Nail Technician with over 1000 hours under my belt. I am currently trying to become licensed in NY State.</p>
+
+ 
+
+<p>I moved back to NY right before hurricane Sandy and soon after became a mom. Having my son turned me into a crazy planner and list person. I have tried too many planners. From cheap 99 cent store ones to really expensive ones. Right now I am using an Erin Condren life planner annual months.  There are so many things to learn when it comes to planning. Bullet planning  and sticker pages, pen styles and page thickness and more. I’m still learning new things. But my new favorite thing is sticker planning and making.</p>
+
+ 
+
+<p>I also have been helping friends write resumes for years. This started in High School. So back then I was a part of this program called the academy of finance. We learned numerous things to help us get ahead in life and things we should know about business and finance. (hence my infatuation with accounting). After that I became everyone’s  go to girl for resumes.
+
+ 
+
+Im also good at party planning and house keeping.</p>
+		
+		 <form action="contactme.php" method="post">
+	<div>
+		<label>
+			Name:<br /><input type="text" name="Name" required="required" placeholder="Full Name (required)" title="Name is required" tabindex="10" size="44" autofocus />
+		</label>
+	</div>
+	<div>	
+		<label>
+			Email:<br /><input type="email" name="Email" required="required" placeholder="Email (required)" title="A valid email is required" tabindex="20" size="44" />
+		</label>
+	</div>
+	<!-- below change the HTML to your form elements - only 'Name' & 'Email' (above) are significant -->
+	<div>	
+		<label>
+			Comments:<br /><textarea name="Comments" cols="36" rows="4" placeholder="Your comments are important to us!" tabindex="30"></textarea>
+		</label>
+	</div>	
+	<div></div>
+    <div class="g-recaptcha" data-sitekey="6LeAOUAUAAAAAJFhipRUMnFiYScubpATrIuDfO5u"></div> 
+	<div>
+		<input type="submit" value="submit" />
+	</div>
+    </form>
+	<!-- END HTML FORM -->
+    <script type="text/javascript"
+        src="https://www.google.com/recaptcha/api.js?hl=en">
+    </script>
+	 <p class="clear-recaptcha"></p>
+     </main>
+	</div>
+	<div id="columnB">
+		<h2>Recent Updates</h2>
+		<p><strong>[08/19/2018]</strong> For the last few years there’s been a fast-growing phenomenon of people using paper planners again. Planner enthusiasts share and document the way they use their planners which combines scrapbooking, journaling, list-making and organization. Planner addicts are often very loyal to their planner of choice. Weekly layouts are decorated with paper die-cuts, stickers, washi tape and rubber stamps – often made specifically for their planner of choice. Layouts and organization tips are shared on Instagram, in Facebook Groups and in Youtube Videos. It’s a big thing.</p>
+		<p><strong>[08/17/2018]</strong> In the digital age, there’s almost no way to go completely off the grid or tech-free, and frankly, the capabilities of phones offer us levels of safety and information that weren’t as available or accessible in the past. But if cutting down on screen time is a goal, or sounds appealing, picking up a paper planner might be a good place to start.</p>
+		<p><strong>[07/03/2018]</strong> Dip powder nails are somewhere between a regular mani and a fake nail. We can consider them a "diet acrylic," says celebrity manicurist Erica Marton. Instead of using UV rays to seal in your polish, the color comes from a pigmented powder. Between base coats and a sealant, you dip your nails into a little jar of your chosen color (SNS and Revel are the two most popular and vetted manufacturers) for a mani that could last three to four weeks. The trend isn't exactly new per se—it's actually been around for years—but the wonderful world of social media is quickly popularizing the process and helping it stage a comeback.
+		<h2>Helpful Links</h2>
+		<ul>
+			<li><a href="https://www.etsy.com/market/craft_planner">Etsy</a></li>
+			<li><a href="https://www.hobbylobby.com/Scrapbook-Paper-Crafts/Planners-Accessories/c/7-160">Hobby Lobby</a></li>
+			<li><a href="https://stayglam.com/beauty/80-nail-designs-for-short-nails/">Stay Glam</a></li>
+			<li><a href="http://www.galknows.com/cute-nails-design/">Cute Nails</a></li>
+		</ul>
+	</div>
+	<div style="clear: both;">&nbsp;</div>
+</div>
+    </body>
+    
+<div id="footer">
+	<p>Copyright &copy; 2018 Kelly Grimes</p> <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+
+</html>
